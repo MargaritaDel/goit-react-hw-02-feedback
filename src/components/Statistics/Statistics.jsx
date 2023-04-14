3d29dfd97d961469d6f53f2cf44421a0400ff4c1
Margaryta Dieltsova <112723193+MargaritaDel@users.noreply.github.com>
2023-04-14 22:18:25 +0300

    fixed

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,6 @@
 
 import PropTypes from 'prop-types';
+// import { StatisticsList, StatisticsItem } from './Statistics.styled';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
     <div>
