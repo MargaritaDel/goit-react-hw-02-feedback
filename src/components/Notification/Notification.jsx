fbc5c1a94a80435a3cb3479b09728f6b3aa9d3f2
Margaryta Dieltsova <112723193+MargaritaDel@users.noreply.github.com>
2023-04-14 22:06:12 +0300

    1

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-
 import { NotificationStyle } from './Notification.styled';
 const Notification = ({ message }) => <NotificationStyle>{message}</NotificationStyle>;
 
